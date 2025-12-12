df783e351a9fa7cf6658ed25bf2ff4ce9bf86222
DungLa <leanhdung3105@gmail.com>
2025-12-12 11:59:36 +0700

    Sửa hết thư viện

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    server: {
+    open: true // tự động mở browser
+  }
+});
